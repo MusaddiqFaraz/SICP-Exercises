7d8073186fa399dd79747fd12519770658609188
Faraz <musaddiq.faraz1@gmail.com>
2018-10-21 00:26:50 +0530

    added recursive method of multiplication using russian peasant method

---
+++ Chapter 1/Exercise1.17.scm
@@ -0,0 +1,20 @@
+#lang sicp
+
+;;; Exercise 1.17 (Uses Russian peasant method)
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+
+(define (mul a b)
+  (define (mult large small)
+    (cond ((= small 1) large)
+          ((even? small) (mult (double large) (halve small)))
+          (else (+ large (mult large (- small 1))))))
+  (cond ((or (= a 0) (= b 0)) 0)
+        ((< a b) (mult a b))
+        ((< b a) (mult b a))
+        (else (mult a b))))
